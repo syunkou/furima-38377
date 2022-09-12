83b6905a15a76a6ad5a580f4bc0ff6ad4fcab3d9
syunkou <kousyn1113@gmail.com>
2022-09-12 18:56:54 +0900

    商品登録機能修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
-  has_many :purchase_records
+  #has_many :purchase_records
   
   validates :nickname, presence: true
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
