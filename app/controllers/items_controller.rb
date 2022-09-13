1b4bc910558d0ca1837d6bfde4496f2814c2a996
syunkou <kousyn1113@gmail.com>
2022-09-13 17:19:28 +0900

    商品削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
   before_action :move_to_index, except: [:index,:show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
       render new_item_path
     end
   end
- 
+
   def show
   end
 
@@ -39,12 +39,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
-
-
-
-
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
   def item_params
