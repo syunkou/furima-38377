a1b3d3f74b090a983d06d61afc219c720b20ab66
syunkou <kousyn1113@gmail.com>
2022-12-22 06:03:29 +0900

    要素の取得確認

---
+++ app/javascript/ preview.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const formImage = document.getElementById('form-image');
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!formImage) return null;
+  console.log("preview.jsが読み込まれました");
+});
