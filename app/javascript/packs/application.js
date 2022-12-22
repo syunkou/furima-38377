36149b459583e7ad8e9ce90fd7be7c28263d80ed
syunkou <kousyn1113@gmail.com>
2022-12-23 07:40:46 +0900

    ファイル名、id名修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../item_price");
 require("../card.js")
-require('../preview')  
+require("../preview")  
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
