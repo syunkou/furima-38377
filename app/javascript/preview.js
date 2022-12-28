fbd1dd6bb4f20f622345a9cb640ea96a3de0d5dd
syunkou <kousyn1113@gmail.com>
2022-12-29 05:17:06 +0900

    プレビュー画像の表示スペース作成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
   const formImage = document.getElementById('contents-form');
+ // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!formImage) return null;
   console.log("preview.jsが読み込まれました");
