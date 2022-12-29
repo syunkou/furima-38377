4f335443e036713f0e817bdfe68dbc11a1f5cc55
syunkou <kousyn1113@gmail.com>
2022-12-30 06:04:28 +0900

    画像情報の取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,4 +6,12 @@ document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!formImage) return null;
   console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+  });
 }); 
