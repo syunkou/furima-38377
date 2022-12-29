5b30113fa8f546bd3a5ff770d3296e1a1545d1a5
syunkou <kousyn1113@gmail.com>
2022-12-30 06:42:36 +0900

    画像表示追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,16 +5,24 @@ document.addEventListener('DOMContentLoaded', function(){
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!formImage) return null;
-  console.log("preview.jsが読み込まれました");
-
   // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
     console.log(blob);
+
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class','preview');
+    // 表示する画像を生成
+    const previewImage = document.createElement('img')
+    previewImage.setAttribute('class','preview-image')
+    previewImage.setAttribute('src', blob);
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage)
+    previewList.appendChild(previewWrapper)
+
   });
 });
